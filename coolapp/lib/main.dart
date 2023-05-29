d44b77dd165cbbc57ef8f53e175b8ea17544c856
MDMohaymenUlAnam <78275354+MDMohaymenUlAnam@users.noreply.github.com>
2023-05-30 00:14:54 +0600

    Working on the navigationbar

--- coolapp/lib/main.dart
+++ coolapp/lib/main.dart
@@ -9,18 +9,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp( //here the const keyword has to used to solve the warning, so that flutter is happy and wont show errors
+    return MaterialApp(
+      //here the const keyword has to used to solve the warning, so that flutter is happy and wont show errors
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple
-      ),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: const RootPage(),
     ); //widget then argument home, then again widget scaffold and under that another widget. This is the flow of flutter archi. This part basically displays white screen
-  
   }
 }
 
-class RootPage extends StatefulWidget { //when user clicks on the screen and it changes something, we need this stateful widget. Kinda like setstate of React
+class RootPage extends StatefulWidget {
+  //when user clicks on the screen and it changes something, we need this stateful widget. Kinda like setstate of React
   const RootPage({super.key});
 
   @override
@@ -28,10 +27,23 @@ class RootPage extends StatefulWidget { //when user clicks on the screen and it 
 }
 
 class _RootPageState extends State<RootPage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('Floating action button is pressed');
+        },
+        child: const Icon(Icons.home),
+      ),
+      bottomNavigationBar: NavigationBar(destinations: const [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile')
+      ]),
     );
   }
 }
